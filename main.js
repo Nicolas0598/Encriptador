9f7937b592bc99d688b42b6d48473b5f4d3f0ecc
Nicolas <navargasc@gmail.com>
2023-05-29 18:51:44 -0500

    Iniciar

---
+++ main.js
@@ -0,0 +1,35 @@
+const mensajeRecibido=document.querySelector("#mensaje");
+const resultadoMostrado=document.querySelector("#resultado");
+
+const botonE=document.querySelector("#encriptar");
+const botonD=document.querySelector("#desencriptar");
+const botonC=document.querySelector("#copiar")
+
+function encriptar(){
+    var mensaje=mensajeRecibido.value;
+    var mensajeEncriptado=mensaje.replaceAll("e","enter").replaceAll("i","imes").replaceAll("o","ober").replaceAll("a","ai").replaceAll("u","ufat");
+    resultadoMostrado.value=mensajeEncriptado;
+}
+function desencriptar(){
+    var mensaje=mensajeRecibido.value;
+    var mensajeDesencriptado=mensaje.replaceAll("enter","e").replaceAll("imes","i").replaceAll("ober","o").replaceAll("ai","a").replaceAll("ufat","u");
+    resultadoMostrado.value=mensajeDesencriptado
+}
+function limpiar(){
+    document.getElementById("resultado").style.display="inline";
+}
+function limpiar2(){
+    document.getElementById("copiar").style.display="inline";
+}
+function todos(){
+    encriptar();
+    limpiar();
+    limpiar2();
+}
+function copiar(){
+    var mensajeEncriptado=resultadoMostrado.value;
+    navigator.clipboard.writeText(mensajeEncriptado);
+}
+botonE.onclick=todos;
+botonD.onclick=desencriptar;
+botonC.onclick=copiar;
